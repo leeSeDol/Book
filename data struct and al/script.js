cf832ad56090ec0e6e7a5a96c0e0a9b1de81f481
我喜欢雅雅 <3075139617@qq.com>
2017-03-26 22:15:30 +0800

    Update script.js

--- data struct and al/script.js
+++ data struct and al/script.js
@@ -192,6 +192,78 @@ window.onload=function(){
 		return arr;
 	}
 	var nums=Array.matrix(5,5,0);
-	console.log(nums);*/
+	console.log(nums);
 	//处理二维数组中的元素
+	var grades=[[89,77,78],[76,82,81],[91,94,89,99]];
+	var total=0;
+	var average=0.0;
+	for(var row=0;row<grades.length;row++){
+		for(var col=0;col<grades[row].length;col++){
+			total += grades[row][col];
+		}
+		average=total/grades[row].length;
+		console.log("Student "+parseInt(row+1)+" average: "+average.toFixed(2)+" total: "+parseInt(total));
+		total=0;
+		average=0.0;
+	}
+	//参差不齐的数组
+	var grades=[[89,77],[76,82,81],[91,94,89,]];
+	var total=0;
+	var average=0.0;
+	for(var row=0;row<grades.length;row++){
+		for(var col=0;col<grades[row].length;col++){
+			total += grades[row][col];
+		}
+		average=total/grades[row].length;
+		console.log("Student "+parseInt(row+1)+" average: "+average.toFixed(2)+" total: "+parseInt(total));
+		total=0;
+		average=0.0;
+	}*/
+	//对象数组
+	function Point(x,y){
+		this.x=x;
+		this.y=y;
+	}
+	function displayPts(arr){
+		for(var i=0;i<arr.length;i++){
+			console.log(arr[i].x+", "+arr[i].y);
+		}
+	}
+	var p1=new Point(1,2);
+	var p2=new Point(3,5);
+	var p3=new Point(2,8);
+	var p4=new Point(4,4);
+	var points=[p1,p2,p3,p4];
+	for(var i=0;i<points.length;i++){
+		console.log("Point "+parseInt(i+1)+": "+points[i].x+","+points[i].y);
+	}
+	var p5=new Point(12,-3);
+	points.push(p5);
+	displayPts(points);
+	points.shift();
+	displayPts(points);
+	//对象中的数组
+	function weekTemps(){
+		this.dataStore = [];
+	}
+	weekTemps.prototype.add=function(temp){
+		this.dataStore.push(temp);
+	}
+	weekTemps.prototype.average=function(){
+		var total = 0;
+		for(var i=0;i<this.dataStore.length;i++){
+			total += this.dataStore[i];
+		}
+		return total/this.dataStore.length;
+	}
+	var thisWeek = new weekTemps();
+	thisWeek.add(52);
+	thisWeek.add(55);
+	thisWeek.add(61);
+	thisWeek.add(65);
+	thisWeek.add(55);
+	thisWeek.add(50);
+	thisWeek.add(52);
+	thisWeek.add(49);
+	console.log(thisWeek.average());
 };

bd5b83b6cddfa6857d77b0a69de6f169d24d7d5d
我喜欢雅雅 <3075139617@qq.com>
2017-03-27 19:54:51 +0800

    Update script.js

--- data struct and al/script.js
+++ data struct and al/script.js
@@ -266,4 +266,75 @@ window.onload=function(){
 	thisWeek.add(52);
 	thisWeek.add(49);
 	console.log(thisWeek.average());
+	
+		//实现列表类
+	function List(){
+		this.listSize=0;
+		this.pos=0;
+		this.dataStore=[];
+		this.clear=clear;
+		this.find=find;
+		this.toString=toString;
+		this.insert=insert;
+		this.append=append;
+		this.remove=remove;
+		this.front=front;
+		this.end=end;
+		this.prev=prev;
+		this.next=next;
+		this.length=length;
+		this.currPos=currPos;
+		this.moveTo=moveTo;
+		this.getElement=getElement;
+		this.contains = contains;
+	}
+	function append(element){
+		this.dataStore[this.listSize++]=element;
+	}
+	function find(element){
+		for(var i=0;i<this.dataStore.length;i++){
+			if(this.dataStore[i] == element){
+				return i;
+			}
+		}
+		return -1;
+	}
+	function remove(element){
+		var foundAt = this.find(element);
+		if(foundAt > -1){
+			this.dataStore.splice(foundAt,1);
+			--this.listSize;
+			return true;
+		}
+		return false;
+	}
+	function length(){
+		return this.listSize;
+	}
+	function toString(){
+		return this.dataStore;
+	}
+	var names=new List();
+	names.append("Cynthia");
+	names.append("Raymond");
+	names.append("Barbara");
+	console.log(names.toString());
+	names.remove("Raymond");
+	console.log(names.toString());
+	//向列表中添加一个元素
+	function insert(element,after){
+		var insertPos.find(after);
+		if(insertPos > -1){
+			this.dataStore.splice(insertPos+1,0,element);
+			++this.listSize;
+			return true;
+		}
+		return false;
+	}
+	//清空列表中所有的元素
+	function clear(){
+		delete this.dataStore;
+		this.dataStore.length=0;
+		this.listSize=list.pos=0;
+	}
 };

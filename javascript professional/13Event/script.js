33796240105ca800d229dadbb2cfc9ffebda5304
淡定 <3075139617@qq.com>
2017-04-25 17:56:29 +0800

    Update script.js

--- javascript professional/13Event/script.js
+++ javascript professional/13Event/script.js
@@ -1,1 +1,36 @@
+window.onload=function(){
+	/*var EventUtil={
+		addHandler:function(element,type,handler){
+			if(element.addEventListener){
+				element.addEventListener(type,handler,false);
+			}else if(element.attachEvent){
+				element.attachEvent("on"+type,handler);
+			}else{
+				element["on"+type]=handler;
+			}
+		},
+		removeHandler:function(element,type,handler){
+			if(element.removeEventListener){
+				element.removeEventListener(type,handler,false);
+			}else if(element.detachEvent){
+				element.detachEvent("on"+type,handler);
+			}else{
+				element["on"+type]=null;
+			}
+		}
+	};*/
+	var btn=document.getElementById("myBtn");
+	btn.addEventListener("click",handlerDOM2,false);
+	//与addEventListener的参数必须相同，必须为同一个函数名，重写一遍函数没有用
+	btn.removeEventListener("click",handlerDOM2,false);
+	btn.addEventListener("click",handlerDOM2,false);
+	
+	//EventUtil.addHandler(btn,"click",handlerDOM2);
+	function handlerDOM2(event){
+		alert(event.type);
+	};
+};
+function showMessage(){
+	console.log("message");
+}
 
